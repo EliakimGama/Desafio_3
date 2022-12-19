b1e770cff47a504248b404d078494cf6f9feced2
EliakimGama <eliakimgama@hotmail.com>
2022-12-19 17:13:20 -0300

    Criando as classes

---
+++ Pessoa.ts
@@ -0,0 +1,28 @@
+export class Pessoa {
+   
+    private _nome: String;
+    private _cpf: Number;
+    private _telefone: Number;
+
+    public get nome(): String {
+        return this._nome;
+    }
+    public set nome(value: String) {
+        this._nome = value;
+    }
+
+    public get cpf(): Number {
+        return this._cpf;
+    }
+    public set cpf(value: Number) {
+        this._cpf = value;
+    }
+
+    public get elefone(): Number {
+        return this._telefone;
+    }
+    public set telefone(value: Number) {
+        this._telefone = value;
+    }
+
+}
---
+++ Principal.ts
@@ -0,0 +1,11 @@
+import { Pessoa } from "./pessoa";
+
+var pessoa = new Pessoa();
+
+pessoa.nome = "Eliakim Gama";
+pessoa.cpf = 000222333;
+pessoa.telefone = 85999887766;
+
+console.log(pessoa.nome);
+console.log(pessoa.cpf);
+console.log(pessoa.telefone);
