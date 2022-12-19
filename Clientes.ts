bd6fafa4b4b2a1940fe09de07884fa2340018520
EliakimGama <eliakimgama@hotmail.com>
2022-12-19 18:54:33 -0300

    Atualizando...

---
+++ Clientes.ts
@@ -0,0 +1,7 @@
+import { Pessoa } from "./pessoa";
+
+class Cliente extends Pessoa{
+
+    private vip: Boolean;
+
+}
--- Principal.ts
+++ Principal.ts
@@ -3,7 +3,7 @@ import { Pessoa } from "./pessoa";
 var pessoa = new Pessoa();
 
 pessoa.nome = "Eliakim Gama";
-pessoa.cpf = 000222333;
+pessoa.cpf = 44444444;
 pessoa.telefone = 85999887766;
 
 console.log(pessoa.nome);
